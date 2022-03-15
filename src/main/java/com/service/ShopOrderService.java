da75a390171c4ab660758f41e67e73ae603384a8
JiHo Kang <kangjh@impconsulting.co.kr>
2022-03-15 17:20:08 +0900

    간소화작업확인표 개발 v0.01

--- src/main/java/com/service/ShopOrderService.java
+++ src/main/java/com/service/ShopOrderService.java
@@ -1,7 +1,6 @@
 package com.service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -31,41 +30,15 @@ public class ShopOrderService {
 		byte[] data = null;
 		try {
 			JasperReport jasperReport = JasperCompileManager
-					.compileReport(ResourceUtils.getFile(reportPath + "\\test1.jrxml").getAbsolutePath());
+					.compileReport(ResourceUtils.getFile(reportPath + "\\shopOrderSimple.jrxml").getAbsolutePath());
 
 			ObjectMapper objectMapper = new ObjectMapper();
 			List list = objectMapper.readValue(param.get("orderList") + "", ArrayList.class);
-			log.info(param);
 			log.info(list);
+			param.put("OrderListDataSource", new JRMapCollectionDataSource(list));
 			
-			Map<String, Object> parameters = new HashMap<String,Object>();
-			parameters.put("param1", "jhkang");
-			List<Map<String, ?>> paramMaps = new ArrayList<Map<String, ?>>();
-			Map<String, Object> addMap = null;
-			Map<String, Object> addMap2 = null;
-			for(int i = 0; i < 5; i++) {
-				addMap = new HashMap<String, Object>();
-				addMap.put("listName1", "value"+i);
-				addMap.put("listName2", i);
-				paramMaps.add(addMap);
-			}
-			for(int i = 0; i < 5; i++) {
-				addMap2 = new HashMap<String, Object>();
-				addMap2.put("listName1", "value"+i);
-				addMap2.put("listName2", i);
-				paramMaps.add(addMap2);
-			}
-			parameters.put("TestDataSource", new JRMapCollectionDataSource(paramMaps));
-			log.info(parameters);
-	            
-			// 선택된 Shop Order 목록을 데이터 소스에 넣어줌 
-			JRMapCollectionDataSource jrMapCollectionDataSource = new JRMapCollectionDataSource(paramMaps);
-			// 그리고 Map 의 Value중 리스트들도(ex. 라우팅) 데이터 소스로 래핑해서 다시 Entry에 넣어줘야 한다.
-			//param.put("test", "Hello JasperReport");
-			//param.put("name", "jhkang");
-			
-			// Fill the report
-			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
+			// Report에 파라미터 전달
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, param, new JREmptyDataSource());
 
 			data = JasperExportManager.exportReportToPdf(jasperPrint);
 		} catch (Exception e) {
